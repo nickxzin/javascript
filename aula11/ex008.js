32185bde244f9c651c961c9eabf1be5755224cd3
nicolasp <84407787+nickxzin@users.noreply.github.com>
2023-01-16 14:24:41 -0300

    Terminando as condicionais

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 78.2
+console.log(`A velocidade do seu carro ${vel}km/h`)
+if (vel > 60){
+    console.log("Você ultrapassou a velocidade permitida, MULTADO!!")
+
+}
+console.log("Dirija sempre com cinto de segurança!")
